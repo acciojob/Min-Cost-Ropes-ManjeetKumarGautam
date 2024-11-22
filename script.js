32e2c6c25fc898ca56eb57e8f4f765c7998e5c1e
Manjeet Kumar Gautam <86048496+ManjeetKumarGautam@users.noreply.github.com>
2024-11-22 11:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+
+	let totalCost=0;
+	
+	let firstMin=0;
+	let secondMin=0;
+
+	while(arr.length>1){
+		arr.sort();
+		let cost= arr.pop()+arr.pop();
+		totalCost += cost;
+		arr.push(cost);
+		
+	}
+	  return totalCost;
 }
 
 module.exports=mincost;
